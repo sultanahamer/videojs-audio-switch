cbd765028d07902bcec9f4dd49b006cb5070ba36
sultanm <sultanm@sultanms-MacBook-Pro.local>
2023-07-06 23:23:29 +0530

    Bumping videojs version to 8.3.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
 import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/index.ts",
   output: {
     file: "dist/videojs-plugin-audio-switch.js",
-    format: "umd",
+    format: "es",
     globals: {
       "video.js": "videojs",
     },
     sourcemap: true,
   },
-  plugins: [typescript()],
+  plugins: [typescript(), nodeResolve()],
   external: ["video.js"],
 };
